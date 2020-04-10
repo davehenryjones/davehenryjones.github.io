147612580902f84b326b2422fcbac5c41b6a7ad5
Ben Wainwright <43956895+bwainwright72@users.noreply.github.com>
2020-04-10 18:22:47 +0100

    Update index.js

--- pave/js/index.js
+++ pave/js/index.js
@@ -3,6 +3,12 @@
 import {load_vis_nodes} from './vis_nodes.js';
 import {load_data_from_default} from './data_input_handler.js';
 
+// Global variables
+var slider = document.getElementById("daypicker");
+var mymap;
+var earth;
+var services_nodes;
+
 // Loads postcode to co-ordinate data
 function load_grid_ref() {
   var grid_ref = {};
@@ -19,24 +25,33 @@ function load_grid_ref() {
 window.onload = function() {
 
     // initialize the map on the "map" div with a given center and zoom
-    var mymap = L.map('mapid', {
+    mymap = L.map('mapid', {
         center: [51.455, -2.599],
         zoom: 13
     });
 
     // Load map of Bristol
-    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    earth = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
       attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
       maxZoom: 18,
       id: 'mapbox/streets-v11',
       tileSize: 512,
       zoomOffset: -1,
       accessToken: 'pk.eyJ1IjoicGF2ZS1oZWFsdGgiLCJhIjoiY2s3dWQxZHUzMTZlYTNncXR1OHB2NTBkYiJ9.7mf8ut1FJpHCFzcsy7qiDA'
-    }).addTo(mymap);
+    });
+    earth.addTo(mymap);
 
     // Load Data Vis from data
     var grid_ref = load_grid_ref();
-    var services_nodes;
     services_nodes = load_data_from_default(grid_ref);
-    setTimeout(function() {load_vis_nodes(mymap, grid_ref, services_nodes);}, 3000);
+    setTimeout(function() {load_vis_nodes(mymap, services_nodes[0]);}, 3000);
 };
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+  mymap.eachLayer(function (layer) {
+    mymap.removeLayer(layer);
+  });
+  earth.addTo(mymap);
+  load_vis_nodes(mymap, services_nodes[this.value]);
+}
